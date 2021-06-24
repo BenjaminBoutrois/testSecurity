3269c08d7b8b5648c7b30455e4abddb760e81585
BenjaminBoutrois <benjamin.boutrois28@gmail.com>
2021-06-24 10:57:29 +0200

    Ajout champs Prénom et Nom

--- src/main/java/com/objis/demospring/security/domaine/User.java
+++ src/main/java/com/objis/demospring/security/domaine/User.java
@@ -1,9 +1,7 @@
 package com.objis.demospring.security.domaine;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,12 +13,15 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
-public class User implements UserDetails{
+public class User implements UserDetails
+{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
+	private String firstName;
+	private String lastName;
 	private String username;
 	private String password;
 
@@ -29,54 +30,93 @@ public class User implements UserDetails{
 		super();
 	}
 
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public User(String username, String password) {
+	public User(String username, String password)
+	{
 		super();
 		this.username = username;
 		this.password = password;
 	}
 
+	public User(String firstName, String lastName, String username, String password)
+	{
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+		this.password = password;
+	}
+
+	public String getFirstName()
+	{
+		return firstName;
+	}
+
+	public void setFirstName(String firstName)
+	{
+		this.firstName = firstName;
+	}
+
+	public String getLastName()
+	{
+		return lastName;
+	}
+
+	public void setLastName(String lastName)
+	{
+		this.lastName = lastName;
+	}
+
+	public String getUsername()
+	{
+		return username;
+	}
+
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
+
+	public String getPassword()
+	{
+		return password;
+	}
+
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
+
 	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
+	public Collection<? extends GrantedAuthority> getAuthorities()
+	{
 		// TODO Auto-generated method stub
 		return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
 	}
 
 	@Override
-	public boolean isAccountNonExpired() {
+	public boolean isAccountNonExpired()
+	{
 		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
-	public boolean isAccountNonLocked() {
+	public boolean isAccountNonLocked()
+	{
 		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
-	public boolean isCredentialsNonExpired() {
+	public boolean isCredentialsNonExpired()
+	{
 		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
-	public boolean isEnabled() {
+	public boolean isEnabled()
+	{
 		// TODO Auto-generated method stub
 		return true;
 	}
